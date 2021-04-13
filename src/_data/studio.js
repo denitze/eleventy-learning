5895ba57150c641f68ecae70394504b0f203cdb8
denitze <denise.brachwitz@gmail.com>
2021-04-13 10:56:33 +0200

    until lesson 19

--- src/_data/studio.js
+++ src/_data/studio.js
@@ -1,4 +1,4 @@
-const Cache = require("@11ty/eleventy-cache-assets");
+const Cache = require('@11ty/eleventy-cache-assets');
 
 /**
  * Grabs the remote data for studio images and returns back
@@ -9,11 +9,11 @@ const Cache = require("@11ty/eleventy-cache-assets");
 module.exports = async () => {
   try {
     // Grabs either the fresh remote data or cached data (will always be fresh live)
-    const { items } = await Cache(
-      "https://11ty-from-scratch-content-feeds.piccalil.li/media.json",
+    const {items} = await Cache(
+      'https://11ty-from-scratch-content-feeds.piccalil.li/media.json',
       {
-        duration: "1d", // 1 day
-        type: "json",
+        duration: '1d', // 1 day
+        type: 'json'
       }
     );
 
